0fa351dfa6afff4cd7316513b2dae96893fc23d6
dyfok <66250490+dyfok@users.noreply.github.com>
2020-06-03 01:47:16 +0800

    TODO TEMPLATE

--- js/app.js
+++ js/app.js
@@ -1,6 +1,58 @@
-(function (window) {
-	'use strict';
+;(function(){
+	const todos = [
+	{
+		id:1,
+		title:'吃饭',
+		completed:true
+	},
+	{
+		id:2,
+		title:'上课',
+		completed:true
+	},
+	{
+		id:3,
+		title:'上班',
+		completed:true
+	},
+	]
+	new Vue({
+		el:"#app",
+		data:{
+			'title':'TOdos',
+			todos,
+			currentediting:null
+		},
+		methods:{
 
-	// Your starting point. Enjoy the ride!
+			handleNewTodoKeyDown(e){
+				const value = e.target.value.trim()
+				if (!value.length){
+					return
+				}
+				const todos = this.todos
+				todos.push({
+					id: todos.length ? todos[todos.length -1].id +1 : 1,
+					title: value,
+					completed:false
+				})
+				e.target.value = ''
+			},
 
-})(window);
+			handleToggleAllChange(e){
+				const checked = e.target.checked
+				this.todos.forEach(item =>{
+					item.completed = checked
+				})
+			},
+
+			handleToggleDestroy(index){
+				this.todos.splice(index,1)
+			},
+
+			handleGetEditingDblclick(item){
+				 this.currentediting = item
+			}
+		}
+	})
+})()
